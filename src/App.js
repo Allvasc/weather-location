efec6923a99c42304282a699f301c917849c6ecb
Allison Vasconcelos <ah.vasconcelos@gmail.com>
2022-04-24 16:53:52 -0300

    Adição de estilos e imagens

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
+import { WiBarometer, WiHumidity, WiDirectionUp, WiDirectionDown, WiThermometer } from "react-icons/wi";
+import loading from './loading.gif'
 
 function App() {
 
   const [location, setLocation] = useState(false)
   const [weather, setWeather] = useState(false)
 
-  let getWeather = async (lat, long) =>{
-    let res = await axios.get('https://api.openweathermap.org/data/2.5/weather',{
+  let getWeather = async (lat, long) => {
+    let res = await axios.get('https://api.openweathermap.org/data/2.5/weather', {
       params: {
         lat: lat,
         lon: long,
@@ -20,8 +22,6 @@ function App() {
     setWeather(res.data);
   }
 
-
-
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
       getWeather(position.coords.latitude, position.coords.longitude);
@@ -29,33 +29,34 @@ function App() {
     })
   }, [])
 
-  if (location == false) {
+  if (location === false) {
     return (
       <>
-        Você precisa habilitar a localização no browser o/
+        <p>Você precisa habilitar a localização no browser o/</p>
       </>)
-  } else if (weather == false){
+  } else if (weather === false) {
     return (
       <>
-        Carregando o clima
+        <img className='load' src={loading} alt="loading" />
       </>)
   } else {
     return (
       <>
-        <h3>Clima na sua localidade ({weather['weather'][0]['description']})</h3>
-        <hr />
-        <div>
-          <p>Temperatura atual: {weather['main']['temp']}°</p>
-          <p>Temperatura maxima: {weather['main']['temp_max']}°</p>
-          <p>Temperatura minima: {weather['main']['temp_min']}°</p>
-          <p>Pressão: {weather['main']['pressure']} hPa</p>
-          <p>Humidade do ar: {weather['main']['humidity']}%</p>
+        <div className='case'>
+          <h3>Clima na sua localidade <br /> ({weather['weather'][0]['description']})</h3>
+          <hr />
+          <div>
+            <p><WiThermometer className='sizeIcon' />Temperatura atual: {weather['main']['temp']}°</p>
+            <p><WiDirectionUp className='sizeIcon' />Temperatura maxima: {weather['main']['temp_max']}°</p>
+            <p><WiDirectionDown className='sizeIcon' />Temperatura minima: {weather['main']['temp_min']}°</p>
+            <p><WiBarometer className='sizeIcon' /> Pressão atm: {weather['main']['pressure']} hPa</p>
+            <p><WiHumidity className='sizeIcon' />Humidade do ar: {weather['main']['humidity']}%</p>
+          </div>
         </div>
       </>
     );
 
   }
-
 
 }
 
